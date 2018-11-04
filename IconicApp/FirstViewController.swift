8e749d3dfb4a571ee98be98f2f9d9eb828ac3a02
Fabrice Mulumba <fabreezem@gmail.com>
2018-11-04 03:10:11 -0500

    Initial Commit

---
+++ IconicApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IconicApp
+//
+//  Created by Fabrice Mulumba on 11/4/18.
+//  Copyright © 2018 Fabrice Mulumba. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
